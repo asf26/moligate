99feeaf2b5fbe9825c34403964d68c0287ec658e
Gavin <guochangrong2833@163.com>
2026-07-06 15:16:48 +0800

    fix: restore channel root route compatibility

--- router/channel_router_test.go
+++ router/channel_router_test.go
@@ -15,6 +15,7 @@ import (
 func TestChannelStatusRoutesUseOperatePermission(t *testing.T) {
 	assertChannelRoutePermission(t, http.MethodPost, "/:id/status", authz.ChannelOperate, controller.UpdateChannelStatus)
 	assertChannelRoutePermission(t, http.MethodPost, "/status/batch", authz.ChannelOperate, controller.BatchUpdateChannelStatus)
+	assertChannelRoutePermission(t, http.MethodPut, "", authz.ChannelWrite, controller.UpdateChannel)
 	assertChannelRoutePermission(t, http.MethodPut, "/", authz.ChannelWrite, controller.UpdateChannel)
 }
 
@@ -22,9 +23,19 @@ func TestChannelDeleteRoutesUseSensitiveWritePermission(t *testing.T) {
 	assertChannelRoutePermission(t, http.MethodDelete, "/:id", authz.ChannelSensitiveWrite, controller.DeleteChannel)
 	assertChannelRoutePermission(t, http.MethodPost, "/batch", authz.ChannelSensitiveWrite, controller.DeleteChannelBatch)
 	assertChannelRoutePermission(t, http.MethodDelete, "/disabled", authz.ChannelSensitiveWrite, controller.DeleteDisabledChannel)
+	assertChannelRoutePermission(t, http.MethodPut, "", authz.ChannelWrite, controller.UpdateChannel)
 	assertChannelRoutePermission(t, http.MethodPut, "/", authz.ChannelWrite, controller.UpdateChannel)
 	assertChannelRoutePermission(t, http.MethodPut, "/tag", authz.ChannelWrite, controller.EditTagChannels)
 	assertChannelRoutePermission(t, http.MethodPost, "/batch/tag", authz.ChannelWrite, controller.BatchSetChannelTag)
+}
+
+func TestChannelRootRoutesKeepTrailingSlashCompatibility(t *testing.T) {
+	assertChannelRoutePermission(t, http.MethodGet, "", authz.ChannelRead, controller.GetAllChannels)
+	assertChannelRoutePermission(t, http.MethodGet, "/", authz.ChannelRead, controller.GetAllChannels)
+	assertChannelRoutePermission(t, http.MethodPost, "", authz.ChannelSensitiveWrite, controller.AddChannel)
+	assertChannelRoutePermission(t, http.MethodPost, "/", authz.ChannelSensitiveWrite, controller.AddChannel)
+	assertChannelRoutePermission(t, http.MethodPut, "", authz.ChannelWrite, controller.UpdateChannel)
+	assertChannelRoutePermission(t, http.MethodPut, "/", authz.ChannelWrite, controller.UpdateChannel)
 }
 
 func TestChannelStatusRoutesRegisterWithoutConflict(t *testing.T) {
